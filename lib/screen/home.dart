2c87418fe28f7cddf7c41b2e1e65889ad12206a5
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-10-10 10:06:14 +0700

    :hammer: API call code refactoring

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:tutorial_rest_api/model/user.dart';
+import 'package:tutorial_rest_api/services/user_api.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,6 +11,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List<User> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,41 +30,18 @@ class _HomeScreenState extends State<HomeScreen> {
             final user = users[index];
             return ListTile(
               leading: CircleAvatar(child: Text('${index + 1}')),
-              title: Text(user.name.first),
+              title: Text(user.fullName),
               subtitle: Text(user.email),
             );
           }),
-      floatingActionButton: FloatingActionButton(onPressed: fetchUsers),
     );
   }
 
-  void fetchUsers() async {
-    print('fetchUsers called');
-    const url = 'https://randomuser.me/api/?results=10';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    final results = json['results'] as List<dynamic>;
-    final transformed = results.map((e) {
-      final name = UserName(
-        title: e['name']['title'],
-        first: e['name']['first'],
-        last: e['name']['last'],
-      );
+  Future<void> fetchUsers() async {
+    final response = await UserApi.fetchUsers();
 
-      return User(
-        gender: e['gender'],
-        email: e['email'],
-        phone: e['phone'],
-        cell: e['cell'],
-        nat: e['nat'],
-        name: name,
-      );
-    }).toList();
     setState(() {
-      users = transformed;
+      users = response;
     });
-    print('fetchUsers completed');
   }
 }
