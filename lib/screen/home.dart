eb8a1677b48a2c147c73dd02fec484a9e4282bde
Dimas Pondra Oktafianto <77646429+DimasPondra@users.noreply.github.com>
2023-10-10 09:46:54 +0700

    :sparkles: adding model to the API call

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:tutorial_rest_api/model/user.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +24,10 @@ class _HomeScreenState extends State<HomeScreen> {
           itemCount: users.length,
           itemBuilder: (context, index) {
             final user = users[index];
-            final name = user['name']['first'];
-            final email = user['email'];
             return ListTile(
               leading: CircleAvatar(child: Text('${index + 1}')),
-              title: Text(name),
-              subtitle: Text(email),
+              title: Text(user.name.first),
+              subtitle: Text(user.email),
             );
           }),
       floatingActionButton: FloatingActionButton(onPressed: fetchUsers),
@@ -42,8 +41,25 @@ class _HomeScreenState extends State<HomeScreen> {
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+    final transformed = results.map((e) {
+      final name = UserName(
+        title: e['name']['title'],
+        first: e['name']['first'],
+        last: e['name']['last'],
+      );
+
+      return User(
+        gender: e['gender'],
+        email: e['email'],
+        phone: e['phone'],
+        cell: e['cell'],
+        nat: e['nat'],
+        name: name,
+      );
+    }).toList();
     setState(() {
-      users = json['results'];
+      users = transformed;
     });
     print('fetchUsers completed');
   }
